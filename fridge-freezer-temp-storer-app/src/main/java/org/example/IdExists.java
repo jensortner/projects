8e8396ae2c975daadf64c7c123e712cb0d2e9a8f
jens <jens.ortner@gmail.com>
2023-12-07 18:57:17 +0100

    added class and method

---
+++ fridge-freezer-temp-storer-app/src/main/java/org/example/IdExists.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.File;
+
+public class IdExists {
+    /* Method that takes fridge/freezer id and checks if file exists. If not it creates file.
+     *
+     * @param idToBeChecked
+     * @return boolean
+     */
+    public static void IdCheck(String idToBeChecked){
+        File file = new File();
+        if (file.isFile(idToBeChecked)){
+
+        } else{
+
+        }
+
+    }
+}
--- fridge-freezer-temp-storer-app/src/main/java/org/example/Main.java
+++ fridge-freezer-temp-storer-app/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Get fridge/freezer id.
+        Scanner scanner = new Scanner();
+        System.out.println("Enter freezer/fridge ID");
+        String id = scanner.next();
+        System.out.println("Enter current temperature");
+        long temp = scanner.nextLong();
+
+        //check if id exists
+        boolean idExists = IdExists(id);
+
+
+
     }
 }
