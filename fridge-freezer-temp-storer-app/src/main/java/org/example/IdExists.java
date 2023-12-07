d3d7f83ec7f51622f53a4612c7ed59d1694b1850
jens <jens.ortner@gmail.com>
2023-12-07 19:59:50 +0100

    Fixing issus with IdCheck method

--- fridge-freezer-temp-storer-app/src/main/java/org/example/IdExists.java
+++ fridge-freezer-temp-storer-app/src/main/java/org/example/IdExists.java
@@ -11,7 +11,7 @@ public class IdExists {
     public static void IdCheck(String idToBeChecked){
         File file = new File();
         if (file.isFile(idToBeChecked)){
-
+            
         } else{
 
         }
